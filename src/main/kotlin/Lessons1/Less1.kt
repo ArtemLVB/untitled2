59476382a45bb8fedce8ac4ec6ff0b510f35b20d
HAIER <dhaus403@gmail.com>
2025-04-04 11:03:34 +0300

    Less1-part1 smth

---
+++ src/main/kotlin/Lessons1/Less1.kt
@@ -0,0 +1,11 @@
+package org.example.Lessons1
+
+fun main() {
+    // Объявляем переменные
+    val totalOrderCount: Int = 75
+    val thankYouMessage: String = "Спасибо за вашу покупку!а"
+
+    // Выводим значения переменных
+    println("Количество заказов: $totalOrderCount")
+    println(thankYouMessage)
+}
