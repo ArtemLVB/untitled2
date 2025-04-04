14916487020acdca04788534156a0a2aed784002
HAIER <dhaus403@gmail.com>
2025-04-04 11:09:31 +0300

    Less1-part1 smth

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,13 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    // Объявляем переменные
+    val totalOrderCount: Int = 75
+    val thankYouMessage: String = "Спасибо за вашу покупку!Ждём вас снова"
 
+    // Выводим значения переменных
+    println("Количество заказов: $totalOrderCount")
+    println(thankYouMessage)
 }
+
+
